bf2693e1804241d4f7579ef31fe32ed5e66460b0
Alp Sayin <alpsayin@gmail.com>
2013-04-24 00:24:20 +0300

    give editors access to admin panel for tags

--- taggit/admin.py
+++ taggit/admin.py
@@ -14,6 +14,15 @@ class TagAdmin(admin.ModelAdmin):
         TaggedItemInline
     ]
 
+    def has_add_permission(self, request):
+        return request.user.groups.filter(name='Editors').exists()
+
+    def has_change_permission(self, request, obj=None):
+        return request.user.groups.filter(name='Editors').exists()
+
+    def has_delete_permission(self, request, obj=None):
+        return request.user.groups.filter(name='Editors').exists()
+
 
 admin.site.register(Tag, TagAdmin)
 admin.site.register(TaggedItem)
